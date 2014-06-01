2aa8cc9efb31a7e2b08da31a14ed0bccebad755b
Adam Malek <koozunek@gmail.com>
2014-06-01 11:58:44 +0200

    nowe klasy

---
+++ skrzyzowanie/skrzyzowanie/skrzyzowanie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skrzyzowanie
+{
+    class Skrzyzowanie
+    {
+        Dictionary<String, Pas> Pasy=new Dictionary<string,Pas>();
+
+
+
+    }
+
+    class Pas
+    {
+        enum kierunek { lewo, prosto, prawo };
+        kierunek kier;
+        int iloscSamochodow;
+        bool czyRuchDozwolony;
+        int kosztWlaczenia;
+    }
+}
