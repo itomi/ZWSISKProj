1e8dd5c12558d62ca459389b6a738f2437736a66
Adam Malek <koozunek@gmail.com>
2014-06-01 18:49:38 +0200

    ywalanie niepotrzebnych otwartych pasów

--- skrzyzowanie/skrzyzowanie/SkrzyzowanieSolver.cs
+++ skrzyzowanie/skrzyzowanie/SkrzyzowanieSolver.cs
@@ -8,7 +8,41 @@ namespace skrzyzowanie
 {
     class SkrzyzowanieSolver
     {
-        public SolverProcess solveSkrzyzowanie(Skrzyzowanie skrzyzowanie) {
+        bool[] wektor = new bool[12];
+
+        public SkrzyzowanieSolver()
+        {
+            for (int i = 0; i<wektor.Length; i++)
+            {
+                wektor[i] = true;
+            }
+        }
+
+        bool[] wywalNiepotrzebneOtware(bool [] wejsciowy)
+        {
+            for (int i = 0; i<this.wektor.Length; i++)
+            {
+                if (!this.wektor[i])
+                {
+                    wejsciowy[i] = false;
+                }
+            }
+            return wejsciowy;
+        }
+
+        void zmienWektor(bool[] wejsciowy)
+        {
+            for (int i = 0; i < this.wektor.Length; i++)
+            {
+                if (wejsciowy[i])
+                {
+                    this.wektor[i] = false;
+                }
+            }
+        }
+
+        public SolverProcess solveSkrzyzowanie(Skrzyzowanie skrzyzowanie)
+        {
             int lightChanges = 0;
             SwiatlaGenerator sw = new SwiatlaGenerator();
             Walidator walidator = new Walidator();
@@ -16,13 +50,17 @@ namespace skrzyzowanie
             while (!skrzyzowanie.isEmpty()) {
                 bool[] nextSwiatlaState = sw.generateNextVec(12);
 
+                nextSwiatlaState = wywalNiepotrzebneOtware(nextSwiatlaState);
+
                 while (!walidator.walidacja(nextSwiatlaState)) {
                     nextSwiatlaState = sw.generateNextVec(12);
+                    nextSwiatlaState = wywalNiepotrzebneOtware(nextSwiatlaState);
                 }
                 Tuple<int,int,int> modificationValues = skrzyzowanie.modifyState(nextSwiatlaState);
                 process.addStep(new SolverStep(nextSwiatlaState, modificationValues));
                 
                 lightChanges++;
+                zmienWektor(nextSwiatlaState);
             }
             return process;
         }
